e5285656083393c63df19b644ecc9771c281ada3
kikisan <kiketdule@gmail.com>
2024-11-29 06:10:29 +0100

    test

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className="flex flex-col min-h-svh ">
-          {/* <Header /> */}
+          <Header />
           <Toaster position="top-center" richColors expand={true} />
           <main className="w-full grow">{children}</main>
           <Separator />
